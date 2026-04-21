5f9a538e60884178fd2a773dc03a9c4254c44211
Leonsk5 <leonsk.55@gmail.com>
2026-04-21 04:03:43 -0300

    segundo commit - Projeto Assistente de IA

---
+++ Assistente_IANA_OK/assistente-backend/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const API_KEY = "SUA_API_KEY";
+
+app.post("/api", async (req, res) => {
+    try {
+        const response = await fetch("https://api.groq.com/openai/v1/chat/completions", {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + API_KEY,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(req.body)
+        });
+
+        const data = await response.json();
+        res.json(data);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Erro no servidor" });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando em http://localhost:3000");
+});
